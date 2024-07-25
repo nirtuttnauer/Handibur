a2963e62b4ed626dd8e5b484729c70dc694cdd40
Nir Tuttnauer <nir.tuttnauer@gmail.com>
2024-07-25 15:46:16 +0300

    chore: Update TensorFlow Lite model path in camerascreen.tsx

--- app/(chat)/(vchat)/camerascreen.tsx
+++ app/(chat)/(vchat)/camerascreen.tsx
@@ -46,7 +46,7 @@ export default function App(): React.ReactNode {
 
   // Load the TensorFlow Lite model
   const model = useTensorflowModel(
-    require("@/assets/models/od.tflite"),
+    require("@/assets/models/model.tflite"),
   );
   const actualModel = model.state === "loaded" ? model.model : undefined;
   console.log(model.model);
