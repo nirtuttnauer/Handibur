ffb33044bc12e3878c797eca278648970f3fc2b2
Veviks <eva.karkar@gmail.com>
2024-08-07 22:44:32 +0300

    CSS: adding the design of the login-register screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,6 +1,6 @@
 'use strict';
 import React, { useEffect, useState } from 'react';
-import { TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { TextInput, TouchableOpacity, StyleSheet,Image, ImageBackground } from 'react-native';
 import { View, Text } from '@/components/Themed';
 import { useAuth } from '@/context/auth';
 import { useRouter } from 'expo-router';
@@ -28,95 +28,110 @@ const Login = () => {
     }
   };
 
-  return (
-      <View style={styles.container}>
-        <Stack.Screen options={{ headerShown: false }} />
-        <Text style={styles.title}>ברוכים השבים</Text>
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Email"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          placeholderTextColor="gray"
-        />
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-          secureTextEntry
-          placeholderTextColor="gray"
-        />
-        {error ? <Text style={styles.error}>{error}</Text> : null}
-        <TouchableOpacity style={styles.button} onPress={handleLogin} testID="login">
-          <Text style={styles.buttonText}>התחבר</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonSecondary} onPress={() => router.push('/register')}>
-          <Text style={styles.buttonText}>הרשמה</Text>
-        </TouchableOpacity>
-      </View>
-  );
+  return (    <View style={styles.container}>
+    <Stack.Screen options={{ headerShown: false }} />
+    <Image source={require('@/assets/images/LOGO.png')} style={styles.logo} />
+    <Text style={styles.title}>ברוכים הבאים!</Text>
+    {error ? <Text style={styles.error}>{error}</Text> : null}
+    <TextInput
+      style={styles.input}
+      value={email}
+      onChangeText={setEmail}
+      placeholder="Email"
+      keyboardType="email-address"
+      autoCapitalize="none"
+      placeholderTextColor="gray"
+    />
+    <TextInput
+      style={styles.input}
+      value={password}
+      onChangeText={setPassword}
+      placeholder="Password"
+      secureTextEntry
+      placeholderTextColor="gray"
+    />
+    <TouchableOpacity style={styles.buttonPrimary} onPress={handleLogin} testID="login">
+      <Text style={styles.buttonTextPrimary}>התחברות</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.buttonSecondary} onPress={() => router.push('/register')}>
+      <Text style={styles.buttonTextSecondary}>הרשמה</Text>
+    </TouchableOpacity>
+  </View>
+);
 };
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: 'cover',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: 'white',
-    marginBottom: 24,
-  },
-  input: {
-    width: '100%',
-    height: 50,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 16,
-    paddingHorizontal: 16,
-    borderRadius: 8,
-    backgroundColor: 'white',
-    color: 'black',
-  },
-  error: {
-    color: 'red',
-    textAlign: 'center',
-    marginBottom: 12,
-  },
-  button: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#007BFF',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-    marginTop: 16,
-  },
-  buttonSecondary: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#6C757D',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-    marginTop: 16,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+container: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: 16,
+  backgroundColor: '#F7F8FA',
+},
+logoContainer: {
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginBottom: 24,
+},
+logo: {
+  width: 150,
+  height: 150,
+  resizeMode: 'contain',
+},
+title: {
+  fontSize: 24,
+  fontWeight: 'bold',
+  color: '#2E6AF3',
+  marginBottom: 40,
+  marginTop: 70,
+},
+input: {
+  width: '100%',
+  height: 50,
+  borderColor: '#CCCCCC',
+  borderWidth: 1,
+  marginBottom: 16,
+  paddingHorizontal: 16,
+  borderRadius: 8,
+  backgroundColor: 'white',
+  color: 'black',
+},
+error: {
+  color: 'red',
+  textAlign: 'center',
+  marginBottom: 12,
+},
+buttonPrimary: {
+  width: '100%',
+  height: 50,
+  backgroundColor: '#2E6AF3',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 8,
+  marginTop: 16,
+},
+buttonSecondary: {
+  width: '100%',
+  height: 50,
+  backgroundColor: 'white',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 8,
+  marginTop: 16,
+  borderWidth: 1,
+  borderColor: '#CCCCCC',
+},
+buttonTextPrimary: {
+  color: 'white',
+  fontSize: 16,
+  fontWeight: 'bold',
+},
+buttonTextSecondary: {
+  color: 'black',
+  fontSize: 16,
+  fontWeight: 'bold',
+},
 });
+
 
 export default Login;
